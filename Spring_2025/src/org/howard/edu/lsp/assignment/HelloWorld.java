3c3c82aadb517be5911bf5fb7e58ddc0606a3bad
NoahAdeyemiHU26 <127359871+NoahAdeyemiHU26@users.noreply.github.com>
2025-01-21 10:39:36 -0500

    Update HelloWorld.java
    Added my name

--- Spring_2025/src/org/howard/edu/lsp/assignment/HelloWorld.java
+++ Spring_2025/src/org/howard/edu/lsp/assignment/HelloWorld.java
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Noah Adeyemi
 public class HelloWorld {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
